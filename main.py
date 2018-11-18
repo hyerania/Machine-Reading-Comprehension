627ea889dd88ef06b7f30264395383ec71a86f08
Yerania Hernandez <hernandez.yerania2@gmail.com>
2018-11-18 15:36:37 -0600

    Initialized session and model setup

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 # coding: utf-8
+import argparse
+import logging
 import os
+import sys
+import tensorflow as tf
+
 
 from preprocessing import load_json, preprocess
 from embedding import get_glove
 from mrcModel import *
 
+logging.basicConfig(level=logging.INFO)
+
+def modelSetup(sess, model, trainDir):
+	checkpoint = tf.train.get_checkpoint_state(trainDir)
+	path = ""
+	if checkpoint:
+		path = checkpoint.model_checkpoint_path + ".index"
+
+	# Model previously exists
+	if checkpoint and (tf.gfile.Exists(checkpoint.model_checkpoint_path) or tf.gfile.Exists(path)):
+		model.saver.restore(sess, checkpoint.model_checkpoint_path)
+	else: # No saved checkpoints
+		sess.run(tf.global_variables_initializer())
+
+
 
 if __name__ == "__main__":
 	## Static variables
 	data_dir = "./dataset/"
+	train_dir = "./train/"
 
 	# Hyperparameters
 	learning_rate = 0.001
@@ -36,4 +57,23 @@ if __name__ == "__main__":
 	# Initialize model
 	mrcModel = mrcModel(id2word, word2id, embed_matrix)
 
+	# Configuration
+	config = tf.ConfigProto()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--mode")
+	args = parser.parse_args()
 
+	# Train Mode
+	if args.mode == 'train':
+		print("Training Network")
+		logFile = logging.FileHandler(os.path.join(train_dir, "logFile.txt"))
+		logging.getLogger().addHandler(logFile)
+
+		with tf.Session(config = config) as sess:
+			modelSetup(sess, mrcModel, train_dir)
+			print ("Model finished setup")
+			# mrcModel.train()
+
+	# Test Mode
+	elif args.mode == 'test':
+		print("Testing Network")
