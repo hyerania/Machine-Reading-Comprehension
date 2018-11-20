caf517cf1ef75f66106093e545387813a2d6248d
SakhiStuti <stuti.sakhi@gmail.com>
2018-11-20 02:03:33 -0600

    Train Function Update1

---
+++ batcher.py
@@ -0,0 +1,233 @@
+from __future__ import absolute_import
+from __future__ import division
+
+import numpy as np
+import random
+from vocab import PAD_ID, UNK_ID
+
+
+class Batch(object):
+    """A class to hold the information needed for a training batch"""
+
+    def __init__(self, context_ids, context_mask, context_tokens, qn_ids, qn_mask, qn_tokens, ans_span, ans_tokens, uuids=None):
+        """
+        Inputs:
+          {context/qn}_ids: Numpy arrays.
+            Shape (batch_size, {context_len/question_len}). Contains padding.
+          {context/qn}_mask: Numpy arrays, same shape as _ids.
+            Contains 1s where there is real data, 0s where there is padding.
+          {context/qn/ans}_tokens: Lists length batch_size, containing lists (unpadded) of tokens (strings)
+          ans_span: numpy array, shape (batch_size, 2)
+          uuid: a list (length batch_size) of strings.
+            Not needed for training. Used by official_eval mode.
+        """
+        self.context_ids = context_ids
+        self.context_mask = context_mask
+        self.context_tokens = context_tokens
+
+        self.qn_ids = qn_ids
+        self.qn_mask = qn_mask
+        self.qn_tokens = qn_tokens
+
+        self.ans_span = ans_span
+        self.ans_tokens = ans_tokens
+        #IS IT REQUIRED????????????????????????????????????????????
+        self.uuids = uuids
+
+        self.batch_size = len(self.context_tokens)
+
+
+def sentence_to_token_ids(sentence, word2id):
+    """Turns a sentence string into word indices
+    e.g. "i do n't know" -> [9, 32, 16, 96]
+    Note any token that isn't in the word2id mapping gets mapped to the id for UNK
+    Input: 
+        sentence: string
+        word2id: dictionary for word to id mapping
+    Output:
+        tokens: list of words in the sentence
+        ids : list of ids of each word in the sentence
+    """
+    ####WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY??????????????
+    """
+    words = []
+    for space_separated_fragment in sentence.strip().split():
+        words.extend(re.split(" ", space_separated_fragment))
+    return [w for w in words if w]
+    """
+    tokens = sentence.strip().split()
+    ids = [word2id.get(w, UNK_ID) for w in tokens]
+    return tokens, ids
+
+
+def intstr_to_intlist(string):
+    """Given a string e.g. '311 9 1334 635 6192 56 639', returns as a list of integers"""
+    return [int(s) for s in string.split()]
+
+
+def padded(token_batch, batch_pad):
+    """
+    Inputs:
+      token_batch: List (length batch size) of lists of ints.
+      batch_pad: Int. Length to pad to.
+    Returns:
+      List (length batch_size) of padded lists of ints.
+    """
+    return map(lambda token_list: token_list + [PAD_ID] * (batch_pad - len(token_list)), token_batch)
+
+
+def add_batches(batches, word2id, context_file, qn_file, ans_file, batch_size, context_len, question_len):
+    """
+    Adds batches into the "batches" list.
+    Each time it starts from the where it left the files last time and fills in 160 batches into the batches list
+    It will in nothing when the EOF has reached
+    It will add less than 160 batches when the file has less training samples left
+    Inputs:
+      batches: list to add batches to, empty list
+      word2id: dictionary mapping word (string) to word id (int)
+      context_file, qn_file, ans_file: paths to {train/dev}.{context/question/answer} data files
+      batch_size: int. how big to make the batches
+      context_len, question_len: max length of context and question respectively
+    """
+    print("Adding batches start.....................")
+    examples = [] # list of (context_ids, context_tokens, qn_ids, qn_tokens, ans_span, ans_tokens)
+    context, ques, ans = context_file.readline(), qn_file.readline(), ans_file.readline() # read the next line from each
+    #Each line has a training sample (Context[i], Question[i], Answerspan[i])
+    #Each of context, qn, ans are strings at the moment
+
+    while context and ques and ans: # Keep adding till all the training samples are covered
+
+        # Convert tokens to word ids
+        context_tokens, context_ids = sentence_to_token_ids(context, word2id) #[Number of words in context]
+        ques_tokens, ques_ids = sentence_to_token_ids(ques, word2id) #[Number of words in question]
+        ans_span = intstr_to_intlist(ans) #[2]
+
+        # read the next line from each file
+        context, ques, ans = context_file.readline(), qn_file.readline(), ans_file.readline()
+
+        # get ans_tokens from ans_span
+        assert len(ans_span) == 2
+        if ans_span[1] < ans_span[0]:
+            print("Found an ill-formed gold span: start=%i end=%i" % (ans_span[0], ans_span[1]))
+            continue
+        ans_tokens = context_tokens[ans_span[0] : ans_span[1]+1] #[Number of words in answer]
+
+        # discard too-long questions
+        if len(ques_ids) > question_len or len(context_ids) > context_len:
+            continue
+        
+        
+        # Add the training sample to example
+        examples.append((context_ids, context_tokens, ques_ids, ques_tokens, ans_span, ans_tokens))
+
+        # stop refilling if you have 160 batches
+        if len(examples) == batch_size * 160:
+            break
+
+    # Exits loop if 16 batches have been filled into example or end of file has been reached
+
+    # Sort by question length
+    # Note: if you sort by context length, then you'll have batches which contain the same context many times (because each context appears several times, with different questions)
+    examples = sorted(examples, key=lambda e: len(e[2]))
+
+    # Make into batches and append to the list batches
+    for batch_start in range(0, len(examples), batch_size):
+
+        # Note: each of these is a list length batch_size of lists of ints (except on last iter when it might be less than batch_size)
+        context_ids_batch, context_tokens_batch, ques_ids_batch, ques_tokens_batch, ans_span_batch, ans_tokens_batch = zip(*examples[batch_start:batch_start+batch_size])
+        
+        # Pad context_ids and ques_ids
+        ques_ids = padded(ques_ids, question_len) # pad questions to length question_len
+        context_ids = padded(context_ids, context_len) # pad contexts to length context_len
+
+        # Make ques_ids into a np array and create ques_mask
+        ques_ids = np.array(ques_ids) # [question_len, batch_size/<batchsize]
+        ques_mask = (ques_ids != PAD_ID).astype(np.int32) # [question_len, batch_size/<batchsize]
+
+        # Make context_ids into a np array and create context_mask
+        context_ids = np.array(context_ids) # [context_len, batch_size/<batchsize]
+        context_mask = (context_ids != PAD_ID).astype(np.int32) # [context_len, batch_size/<batchsize]
+
+        # Make ans_span into a np array
+        ans_span = np.array(ans_span) # [batch_size/<batchsize, 2]
+
+        # Make into a Batch object
+        batch = Batch(context_ids, context_mask, context_tokens, ques_ids, ques_mask, ques_tokens, ans_span, ans_tokens)
+        
+        batches.append(batch)
+
+    # shuffle the batches
+    random.shuffle(batches)
+    print("Added ",len(batches)," batches")
+    return
+
+def get_batch_generator(word2id, context_path, qn_path, ans_path, batch_size, context_len, question_len):
+    """This function is a generator. Here is how it runs:
+        1. When the function is called first time, none of the code is run, only a generator is returned
+        2. Now when this object is used in for loop like, 'for i in generator', the whole function runs till reaches yeild
+        3. Every time the state is saved, and it runs from the next iter
+        4. The generator stops returning anything when it can no more reach yield
+    """
+    
+    #Open the files for context, question and answer
+    context_file, qn_file, ans_file = open(context_path), open(qn_path), open(ans_path)
+    
+    batches = []
+
+    while True:
+        #When the generator is used for the first time in for loop or when batches gets empty (after 160*batchsize)
+        if len(batches) == 0: 
+            add_batches(batches, word2id, context_file, qn_file, ans_file, batch_size, context_len, question_len)
+        #When the list is empty and we have reached the end of training files, prevent it from going to yield
+        if len(batches) == 0:
+            break
+        
+        yield batches.pop(0)
+
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mrcModel.py
+++ mrcModel.py
@@ -6,6 +6,8 @@ from tensorflow.python.ops import embedding_ops
 from tensorflow.python.ops import variable_scope as vs
 from layers import Highway, RNNEncoder, BidafAttention, SimpleSoftmaxLayer
 from helperFunctions import masked_softmax
+import logging
+from batcher import get_batch_generator
 
 ### Model
 class mrcModel(object):
@@ -40,7 +42,7 @@ class mrcModel(object):
         self.prob_dropout = tf.placeholder_with_default(1.0, shape=())
 
     def add_embed_layer(self, embed_matrix):
-#         with vs.variable_scope("embedding"):
+        # with vs.variable_scope("embedding"):
         with tf.variable_scope("embedding"):
             embedding_matrix = tf.constant(embed_matrix, dtype=tf.float32, name="embed_matrix")
             self.context_embed = embedding_ops.embedding_lookup(embedding_matrix, self.context_ids)
@@ -102,3 +104,137 @@ class mrcModel(object):
 
             # Total loss
             self.loss = self.loss_start + self.loss_end
+            
+    def run_train_iter(self, session, batch):
+        """
+        This performs a single training iteration (forward pass, loss computation, backprop, parameter update)
+        Inputs:
+          session: TensorFlow session
+          batch: a Batch object
+        Returns:
+          loss: The loss (averaged across the batch) for this batch.
+          global_step: The current number of training iterations we've done
+          param_norm: Global norm of the parameters
+          gradient_norm: Global norm of the gradients
+        """
+        # Match up our input data with the placeholders
+        input_feed = {}
+        #Placeholders used a keys for creating the input_feed dictionary
+        input_feed[self.context_ids] = batch.context_ids
+        input_feed[self.context_mask] = batch.context_mask
+        input_feed[self.qn_ids] = batch.qn_ids
+        input_feed[self.qn_mask] = batch.qn_mask
+        input_feed[self.ans_span] = batch.ans_span
+        #####MISSING attribute------------------------------------------------------------------
+        input_feed[self.keep_prob] = 1.0 - self.FLAGS.dropout # apply dropout
+
+        ########MISSING attribute---------------------------------------------------------------
+        # output_feed contains the things we want to fetch.
+        output_feed = [self.updates, self.summaries, self.loss, self.global_step, self.param_norm, self.gradient_norm]
+
+        # Run the model
+        [_, summaries, loss, global_step, param_norm, gradient_norm] = session.run(output_feed, input_feed)
+
+        return loss, global_step, param_norm, gradient_norm   
+    
+    
+            
+    def train(self, session, train_context_path, train_qn_path, train_ans_path, dev_qn_path, dev_context_path, dev_ans_path):
+        """
+        Main training loop.
+        Inputs:
+          session: TensorFlow session
+          {train/dev}_{qn/context/ans}_path: paths to {train/dev}.{context/question/answer} data files
+        """
+
+        # Print number of model parameters
+        """
+        tic = time.time()
+        params = tf.trainable_variables()
+        num_params = sum(map(lambda t: np.prod(tf.shape(t.value()).eval()), params))
+        toc = time.time()
+        logging.info("Number of params: %d (retrieval took %f secs)" % (num_params, toc - tic))
+        
+        # We will keep track of exponentially-smoothed loss
+        exp_loss = None
+        """
+
+        """
+        # Checkpoint management.
+        # We keep one latest checkpoint, and one best checkpoint (early stopping)
+        checkpoint_path = os.path.join(self.FLAGS.train_dir, "qa.ckpt")
+        bestmodel_dir = os.path.join(self.FLAGS.train_dir, "best_checkpoint")
+        bestmodel_ckpt_path = os.path.join(bestmodel_dir, "qa_best.ckpt")
+        best_dev_f1 = None
+        best_dev_em = None
+
+        """
+        epoch = 0
+
+        #logging.info("Beginning training loop...")
+        #WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY????
+        while self.FLAGS.num_epochs == 0 or epoch < self.FLAGS.num_epochs:
+            epoch += 1
+            #epoch_tic = time.time()
+
+            # Loop over batches
+            for batch in get_batch_generator(self.word2id, train_context_path, train_qn_path, train_ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len):
+
+                # Run training iteration
+                #iter_tic = time.time()
+                loss, global_step, param_norm, grad_norm = self.run_train_iter(session, batch)
+                #iter_toc = time.time()
+                #iter_time = iter_toc - iter_tic
+
+                # Update exponentially-smoothed loss
+                if not exp_loss: # first iter
+                    exp_loss = loss
+                else:
+                    exp_loss = 0.99 * exp_loss + 0.01 * loss
+
+                # Sometimes print info to screen
+                if global_step % self.FLAGS.print_every == 0:
+                    logging.info(
+                        'epoch %d, iter %d, loss %.5f, smoothed loss %.5f, grad norm %.5f, param norm %.5f, batch time %.3f' %
+                        (epoch, global_step, loss, exp_loss, grad_norm, param_norm, iter_time))
+
+                # Sometimes save model
+                if global_step % self.FLAGS.save_every == 0:
+                    logging.info("Saving to %s..." % checkpoint_path)
+                    self.saver.save(session, checkpoint_path, global_step=global_step)
+
+                # Sometimes evaluate model on dev loss, train F1/EM and dev F1/EM
+                if global_step % self.FLAGS.eval_every == 0:
+
+                    # Get loss for entire dev set and log to tensorboard
+                    dev_loss = self.get_dev_loss(session, dev_context_path, dev_qn_path, dev_ans_path)
+                    logging.info("Epoch %d, Iter %d, dev loss: %f" % (epoch, global_step, dev_loss))
+                    write_summary(dev_loss, "dev/loss", summary_writer, global_step)
+
+
+                    # Get F1/EM on train set and log to tensorboard
+                    train_f1, train_em = self.check_f1_em(session, train_context_path, train_qn_path, train_ans_path, "train", num_samples=1000)
+                    logging.info("Epoch %d, Iter %d, Train F1 score: %f, Train EM score: %f" % (epoch, global_step, train_f1, train_em))
+                    write_summary(train_f1, "train/F1", summary_writer, global_step)
+                    write_summary(train_em, "train/EM", summary_writer, global_step)
+
+
+                    # Get F1/EM on dev set and log to tensorboard
+                    dev_f1, dev_em = self.check_f1_em(session, dev_context_path, dev_qn_path, dev_ans_path, "dev", num_samples=0)
+                    logging.info("Epoch %d, Iter %d, Dev F1 score: %f, Dev EM score: %f" % (epoch, global_step, dev_f1, dev_em))
+                    write_summary(dev_f1, "dev/F1", summary_writer, global_step)
+                    write_summary(dev_em, "dev/EM", summary_writer, global_step)
+
+
+                    # Early stopping based on dev EM. You could switch this to use F1 instead.
+                    if best_dev_em is None or dev_em > best_dev_em:
+                        best_dev_em = dev_em
+                        logging.info("Saving to %s..." % bestmodel_ckpt_path)
+                        self.bestmodel_saver.save(session, bestmodel_ckpt_path, global_step=global_step)
+
+
+            epoch_toc = time.time()
+            logging.info("End of epoch %i. Time for epoch: %f" % (epoch, epoch_toc-epoch_tic))
+
+        sys.stdout.flush()
+        
